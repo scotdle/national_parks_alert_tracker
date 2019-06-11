5f89f0c3bbc79cee49e66373e6bba4b5551c97b8
Scott Caudle <scottcaudlewebsite@gmail.com>
2019-06-11 17:44:03 -0400

    added functionality to access the stateName QSP when the fetch is called from alerts.js

--- functions/token-hider.js
+++ functions/token-hider.js
@@ -6,23 +6,25 @@ var alertEndpoint = "";
 var parkEndpoint = "";
 
 
+
 exports.handler = function getURLS(event, context, callback) {
   // apply our function to the queryStringParameters and assign it to a variable
   // Get env var values defined in our Netlify site UI
   const {api_key, alert_api_url, park_api_url} = process.env;
   // In this example, the API Key needs to be passed in the params with a key of key.
   // We're assuming that the ApiParams var will contain the initial ?
-   alertEndpoint = `${alert_api_url}${api_key}`;
-   parkEndpoint = `${park_api_url}${api_key}`;
+   var stateName =event.queryStringParameters.stateName;
+   alertEndpoint = `${alert_api_url}${stateName}${api_key}`;
+   parkEndpoint = `${park_api_url}${stateName}${api_key}`;
 
 
   // Let's log some stuff we already have.
   console.log("Injecting token to", alert_api_url, park_api_url);
   console.log("logging event.....", event);
   console.log("Constructed URLs are ...", alertEndpoint, parkEndpoint);
-getData();
-};
+  
 
+};
 async function getData(alertsArea, alertHeader) {
 
 
