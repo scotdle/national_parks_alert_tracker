17f3ea7aece8e2d433f5da7df7c9685a2e05867e
Scott Caudle <scottcaudlewebsite@gmail.com>
2019-06-13 18:30:21 -0400

    updated/simplified gulp and updated netlify toml file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
 
 
@@ -10,40 +9,17 @@ var runSequence = require('run-sequence');
 
 // Development Tasks
 // -----------------
+function style(){
+  return gulp.src('./scss/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.stream());
+}
 
-// Start browserSync server
-gulp.task('browserSync', function() {
-    browserSync({
-        server: {
-            baseDir: './',
-            browser: 'chrome',
+function watch() {
 
-        },
-    })
-});
+  gulp.watch('./scss/**/*.scss', style);
 
-gulp.task('sass', function() {
-    return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-        .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
-        .pipe(gulp.dest('css')) // Outputs it in the css folder
-        .pipe(browserSync.reload({ // Reloading with Browser Sync
-            stream: true
-        }));
-});
+}
 
-// Watchers
-/*gulp.task('watch', function() {
-    gulp.watch('scss/!**!/!*.scss', ['sass']);
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/!**!/!*.js', browserSync.reload);
-});*/
-
-
-// Build Sequences
-// ---------------
-
-gulp.task('default', function(callback) {
-    runSequence(['sass', 'browserSync'], /*'watch',*/
-        callback
-    );
-});
+exports.default = watch;
